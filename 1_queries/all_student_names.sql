d86d6f1c6f802ac65eba5b0e58d8386dcb442a0f
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-03-06 17:16:30 -0500

    add 2_queries_join for new queries with multiple tables

--- 1_queries/all_student_names.sql
+++ 1_queries/all_student_names.sql
@@ -2,3 +2,7 @@ SELECT id, name
 FROM students 
 WHERE cohort_id = 1
 ORDER BY name;
+
+SELECT students.name as student_name, cohorts.name as cohort_name, cohorts.start_date as cohorts_start_date, students.start_date as student_start_date
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.start_date <> students.start_date;
